9f95cffc985d8e4cab8c25902eb719e1ea796bff
Shawn Siefkas <shawn.siefkas@meredith.com>
2013-12-10 15:15:59 -0600

    Exporting packages

--- rpmbuild.py
+++ rpmbuild.py
@@ -3,10 +3,11 @@
 """Docker rpmbuild.
 
 Usage:
-    docker-packager --spec=<file> --source=<tarball> <image>
+    docker-packager --spec=<file> --source=<tarball> [--output=<path>] <image>
 
 Options:
     -h --help           Show this screen.
+    --output=<path>     Output directory for RPMs [default: .].
     --source=<tarball>  Tarball containing package sources.
     --spec=<file>       RPM Spec file to build.
 
@@ -57,6 +58,13 @@ class Packager(object):
     def __str__(self):
         return self.image
 
+    def export_package(self, output):
+        for diff in client.diff(self.container):
+            if diff['Path'].startswith('/rpmbuild'):
+                if diff['Path'].endswith('.rpm'):
+                    resource = '%s:%s' % (self.container['Id'], diff['Path'])
+                    check_call(['docker', 'cp', resource, output])
+
     def prepare_context(self, source, spec):
         shutil.copy(source, self.context)
         shutil.copy(spec, self.context)
@@ -64,7 +72,7 @@ class Packager(object):
         with open(os.path.join(self.context, 'Dockerfile'), 'w') as f:
             f.write(dockerfile)
 
-    def build(self, source, spec):
+    def build(self, source, spec, output):
         self.prepare_context(source, spec)
         self.image, logs = client.build(self.context)
         print logs
@@ -79,14 +87,16 @@ class Packager(object):
 
         # Hack until https://github.com/dotcloud/docker-py/pull/105 is merged
         check_call(['docker', 'logs', self.container['Id']])
-    
+
+        self.export_package(output)
+
 
 def main():
     args = docopt(__doc__, version='Docker Packager 0.0.1')
 
     try:
         with Packager(args['<image>']) as p:
-            p.build(args['--source'].strip(), args['--spec'].strip())
+            p.build(args['--source'], args['--spec'], args['--output'])
 
     except PackagerException:
         print >> sys.stderr, 'Container build failed!'
